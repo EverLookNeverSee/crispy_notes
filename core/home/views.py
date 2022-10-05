a3df8b86a2e82788fba799c2af2e18bbbe0f2736
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-10-05 12:34:51 +0330

    Implementing index, about and contact views

--- core/home/views.py
+++ core/home/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.views.generic.base import TemplateView
+from django.views.generic.edit import FormView
+from .forms import ContactForm
 
-# Create your views here.
+
+class IndexPageView(TemplateView):
+    template_name = "home/index.html"
+
+
+class AboutPageView(TemplateView):
+    template_name = "home/about.html"
+
+
+class ContactPageView(FormView):
+    template_name = "home/contact.html"
+    form_class = ContactForm
+    success_url = "/"
