935c080b418a3b5b46224d20a34935cd36456274
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-10-07 18:14:41 +0330

    Adding success message to contact view and fixing the inheritance ordering

--- core/home/views.py
+++ core/home/views.py
@@ -1,5 +1,6 @@
 from django.views.generic.base import TemplateView
-from django.views.generic.edit import FormView
+from django.views.generic.edit import CreateView
+from django.contrib.messages.views import SuccessMessageMixin
 from .forms import ContactForm
 
 
@@ -11,7 +12,8 @@ class AboutPageView(TemplateView):
     template_name = "home/about.html"
 
 
-class ContactPageView(FormView):
+class ContactPageView(SuccessMessageMixin, CreateView):
     template_name = "home/contact.html"
     form_class = ContactForm
     success_url = "/"
+    success_message = "Your contact form sent successfully."
