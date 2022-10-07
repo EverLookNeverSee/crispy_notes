884f059caf0b508e0c4573a34c7853ce9c27f6fd
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-10-07 18:19:28 +0330

    Adding User and Profile model to admin panel

--- core/accounts/admin.py
+++ core/accounts/admin.py
@@ -1,3 +1,49 @@
 from django.contrib import admin
+from .models import User, Profile
+from django.contrib.auth.admin import UserAdmin
+
 
 # Register your models here.
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ("email", "is_superuser", "is_active", "is_verified")
+    list_filter = ("email", "is_superuser", "is_active", "is_verified")
+    search_fields = ("email",)
+    ordering = ("email",)
+    fieldsets = (
+        ("Authentication", {"fields": ("email", "password")}),
+        (
+            "permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_superuser",
+                    "is_staff",
+                    "is_verified",
+                )
+            },
+        ),
+        ("group_permissions", {"fields": ("groups", "user_permissions")}),
+        ("important_dates", {"fields": ("last_login",)}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "password1",
+                    "password2",
+                    "is_active",
+                    "is_superuser",
+                    "is_staff",
+                    "is_verified",
+                ),
+            },
+        ),
+    )
+
+
+admin.site.register(Profile)
+admin.site.register(User, CustomUserAdmin)
