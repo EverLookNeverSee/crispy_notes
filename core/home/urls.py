65cb723c6b508516f70b7046ae011612a11fe9b8
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-10-07 20:22:46 +0330

    Caching home app url paths

--- core/home/urls.py
+++ core/home/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
+from django.views.decorators.cache import cache_page
 from .views import IndexPageView, AboutPageView, ContactPageView
 
 app_name = "home"
 
 urlpatterns = [
-    path("", IndexPageView.as_view(), name="index"),
-    path("about/", AboutPageView.as_view(), name="about"),
-    path("contact/", ContactPageView.as_view(), name="contact"),
+    path("", cache_page(timeout=60*60)(IndexPageView.as_view()), name="index"),
+    path("about/", cache_page(timeout=60*60)(AboutPageView.as_view()), name="about"),
+    path("contact/", cache_page(timeout=60*60)(ContactPageView.as_view()), name="contact"),
 ]
