f3f3c6224b9ffbeea086b86a269cb3268a852e7a
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-10-09 19:10:46 +0330

    Implementing email validation on forgot password

--- core/accounts/forms.py
+++ core/accounts/forms.py
@@ -1,4 +1,5 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, PasswordResetForm
+from django.forms import forms
 from .models import User
 
 
@@ -6,3 +7,11 @@ class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ["email", "password1", "password2"]
+
+
+class EmailValidationOnForgotPassword(PasswordResetForm):
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if not User.objects.filter(email__iexact=email, is_verified=True).exists():
+            raise forms.ValidationError("There is no user registered with this email address!")
+        return email
