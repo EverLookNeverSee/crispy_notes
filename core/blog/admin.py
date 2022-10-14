99777fee453e4760eb7f4d5c3844a8606f740c98
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-10-14 23:30:34 +0330

    Adding post and category models to admin panel

--- core/blog/admin.py
+++ core/blog/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Post, Category
+
 
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        "author",
+        "title",
+        "ok_to_publish",
+        "login_required",
+        "created_at",
+        "publish_date",
+    )
+    search_fields = ("title", "content")
+    ordering = ("-created_at",)
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category)
