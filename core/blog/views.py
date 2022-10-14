405e0554873fc8196bfefaa5f585a68fcd9fa13c
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-10-14 23:32:51 +0330

    Implementing blog index view

--- core/blog/views.py
+++ core/blog/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Post, Category
 
-# Create your views here.
+
+class BlogIndexView(ListView):
+    template_name = "blog/blog-index.html"
+    model = Post
+    queryset = Post.objects.filter(ok_to_publish=True, login_required=False)
+    context_object_name = "posts"
+    paginate_by = 9
