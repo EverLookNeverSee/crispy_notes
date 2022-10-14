79209b69ed0a34a995a813ad924f8fbb1eb1f5f1
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-10-14 23:31:42 +0330

    Implementing get method for indexPageView

--- core/home/views.py
+++ core/home/views.py
@@ -2,10 +2,23 @@ from django.views.generic.base import TemplateView
 from django.views.generic.edit import CreateView
 from django.contrib.messages.views import SuccessMessageMixin
 from .forms import ContactForm
+from blog.models import Post, Category
 
 
 class IndexPageView(TemplateView):
     template_name = "home/index.html"
+
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        normal_posts = Post.objects.filter(ok_to_publish=True, login_required=False)[:7]
+        recommended_posts = Post.objects.filter(ok_to_publish=True, login_required=False)[10:13]
+        top_viewed_posts = Post.objects.filter(ok_to_publish=True, login_required=False).order_by("-n_views")[:3]
+        categories = Category.objects.all()
+        context["normal_posts"] = normal_posts
+        context["recommended_posts"] = recommended_posts
+        context["top_viewed_posts"] = top_viewed_posts
+        context["categories"] = categories
+        return self.render_to_response(context)
 
 
 class AboutPageView(TemplateView):
