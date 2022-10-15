667e16a15a8e4f98fb707ba53ce3859871901d23
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-10-15 19:43:20 +0330

    Defining post-detail, category, author and search url paths

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -1,9 +1,19 @@
 from django.urls import path
-from .views import BlogIndexView
+from .views import (
+    BlogIndexView,
+    PostDetailView,
+    CategoryFilterPostListView,
+    AuthorFilterPostListView,
+    SearchPostListView,
+)
 
 
 app_name = "blog"
 
 urlpatterns = [
     path("", BlogIndexView.as_view(), name="index"),
+    path("post/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
+    path("category/<str:cat_name>/", CategoryFilterPostListView.as_view(), name="category"),
+    path("author/<str:author_email>/", AuthorFilterPostListView.as_view(), name="author"),
+    path("search/", SearchPostListView.as_view(), name="search"),
 ]
