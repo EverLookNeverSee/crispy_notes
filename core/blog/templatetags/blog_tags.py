68e4f1be8492cebd4030e72f3c2e8ef92874a5dd
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-10-15 19:42:42 +0330

    Implementing custom bog template tags

---
+++ core/blog/templatetags/blog_tags.py
@@ -0,0 +1,16 @@
+from django import template
+from ..models import Post, Category
+
+register = template.Library()
+
+
+@register.inclusion_tag(filename="home/footer_recommended_posts.html")
+def footer_recommended_posts():
+    recommended_posts = Post.objects.filter(ok_to_publish=True, login_required=False)[10:13]
+    return {"frp": recommended_posts}
+
+
+@register.inclusion_tag(filename="home/footer_all_categories.html")
+def footer_all_categories():
+    all_cats = Category.objects.all()
+    return {"categories": all_cats}
