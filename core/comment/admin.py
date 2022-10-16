e0bf20e6620aa5eae67c616fe9204278a5fff070
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-10-16 21:21:13 +0330

    Adding comment to admin panel

--- core/comment/admin.py
+++ core/comment/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Comment
 
-# Register your models here.
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'post', 'created_at', 'is_approved')
+    list_filter = ('is_approved', 'created_at')
+    search_fields = ('name', 'email', 'message')
