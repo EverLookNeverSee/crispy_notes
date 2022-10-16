d9f443599bb6ec2a30a2a31186dd3c342ccd02db
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-10-16 21:11:19 +0330

    Implementing Comment model

--- core/comment/models.py
+++ core/comment/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from blog.models import Post
 
-# Create your models here.
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    message = models.TextField()
+    is_approved = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+        verbose_name = 'Comment'
+        verbose_name_plural = 'Comments'
+
+    def __str__(self):
+        return f"{self.name} - {self.email}"
