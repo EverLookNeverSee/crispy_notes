4b37cf24b37a140c1ae55332f275944e5ae7f43e
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-10-17 16:51:51 +0330

    Refactoring and restyling

--- core/comment/admin.py
+++ core/comment/admin.py
@@ -4,6 +4,6 @@ from .models import Comment
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'post', 'created_at', 'is_approved')
-    list_filter = ('is_approved', 'created_at')
-    search_fields = ('name', 'email', 'message')
+    list_display = ("name", "email", "post", "created_at", "is_approved")
+    list_filter = ("is_approved", "created_at")
+    search_fields = ("name", "email", "message")
