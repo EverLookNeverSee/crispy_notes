bdf5a193269dc80c42235384b48aa14c68f8f4ea
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-10-17 20:41:35 +0330

    Adding robots url path

--- core/core/urls.py
+++ core/core/urls.py
@@ -32,9 +32,10 @@ urlpatterns = [
     path("", include("home.urls")),
     path("accounts/", include("accounts.urls")),
     path("blog/", include("blog.urls")),
-    path("comment", include("comment.urls")),
+    path("comment/", include("comment.urls")),
     path("summernote/", include("django_summernote.urls")),
     path("sitemap.xml", sitemap, {"sitemaps": sitemaps}, name="django.contrib.sitemaps.views.sitemap"),
+    path("robots.txt", include("robots.urls")),
 
 ]
 
