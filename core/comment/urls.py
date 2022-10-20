7aca25d25cf6dc8d728e87f48f976eef22959de0
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-10-20 19:23:52 +0330

    Defining comment api v1 url branch

--- core/comment/urls.py
+++ core/comment/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from .views import CommentCreateView
 
 app_name = "comment"
 
 urlpatterns = [
     path("<int:pk>/add/", CommentCreateView.as_view(), name="create"),
+    path("api/v1/", include("comment.api.v1.urls")),
 ]
