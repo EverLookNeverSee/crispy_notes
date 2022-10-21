f3fa4761d45f8360403492291faa2a71030e0641
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-10-21 22:23:51 +0330

    Implementing necessary fixtures

--- core/comment/api/v1/tests.py
+++ core/comment/api/v1/tests.py
@@ -8,3 +8,55 @@ from blog.models import Post, Category
 from accounts.models import Profile
 
 User = get_user_model()
+
+
+@pytest.fixture()
+def api_client():
+    client = APIClient()
+    return client
+
+
+@pytest.fixture()
+@pytest.mark.django_db
+def active_user():
+    user = User.objects.create_user(
+        email="active_user@test.com", password="A@123456", is_verified=True
+    )
+    user.save()
+    return user
+
+
+@pytest.fixture()
+@pytest.mark.django_db
+def sample_category():
+    sample_cat = Category.objects.create(name="Sample_Cat")
+    return sample_cat
+
+
+@pytest.fixture()
+@pytest.mark.django_db
+def sample_post(active_user, sample_category):
+    sample_post = Post.objects.create(
+        author=Profile.objects.get(user__id=active_user.id),
+        title="Test post",
+        content="This is the test post content.",
+        ok_to_publish=True,
+        publish_date=timezone.now(),
+    )
+    sample_post.category.set([sample_category])
+    sample_post.save()
+    return sample_post
+
+
+@pytest.fixture()
+@pytest.mark.django_db
+def sample_comment(active_user, sample_post, sample_category):
+    profile = Profile.objects.get(user__id=active_user.id)
+    sample_comment = Comment.objects.create(
+        post=sample_post,
+        name=f"{profile.first_name} {profile.last_name}",
+        email=active_user.email,
+        message="This is the comment message.",
+    )
+    sample_comment.save()
+    return sample_comment
