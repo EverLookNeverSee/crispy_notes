1036fb6f453edbc8b74fe4950907bc6ad481441d
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2022-10-21 15:33:46 +0330

    Implementing necessary fixtures

--- core/accounts/api/v2/tests.py
+++ core/accounts/api/v2/tests.py
@@ -5,3 +5,32 @@ from django.contrib.auth.views import get_user_model
 from rest_framework_simplejwt.tokens import RefreshToken
 
 User = get_user_model()
+
+
+@pytest.fixture()
+def api_client():
+    client = APIClient()
+    return client
+
+
+@pytest.fixture()
+@pytest.mark.django_db
+def common_user():
+    user = User.objects.create_user(
+        email="test_user@test.com",
+        password="A@123456",
+    )
+    user.save()
+    return user
+
+
+@pytest.fixture()
+@pytest.mark.django_db
+def active_user():
+    user = User.objects.create_user(
+        email="active_user@test.com",
+        password="A@123456",
+        is_verified=True
+    )
+    user.save()
+    return user
